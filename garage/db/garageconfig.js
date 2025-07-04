e22fba60c9fc1d53538654f8bd9ff302dc092adf
JEET-HACKS <jitendrasuthar342@gmail.com>
2025-07-04 17:57:23 +0530

    Update garageconfig.js
    Connection string edit

--- garage/db/garageconfig.js
+++ garage/db/garageconfig.js
@@ -1,8 +1,17 @@
- const mongoose=require('mongoose');
+// const mongoose=require('mongoose');
 // mongoose.connect('mongodb://localhost:27017/Garage');
-mongoose.connect(process.env.MONGODB_URI, {
+// mongoose.connect(process.env.MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then(() => console.log('✅ Connected to MongoDB'))
+// .catch((err) => console.error('❌ Connection error:', err));
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb+srv://jitendra:Jitendra00%40%21@jeet-garage.cuwdwtq.mongodb.net/Garage?retryWrites=true&w=majority', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 })
-.then(() => console.log('✅ Connected to MongoDB'))
-.catch((err) => console.error('❌ Connection error:', err));
+.then(() => console.log('Connected to MongoDB Atlas'))
+.catch(err => console.error('Connection error:', err));
