d82a874f9fca51fa01d7184e42697a3eb242d3c5
JEET-HACKS <jitendrasuthar342@gmail.com>
2025-07-04 15:02:34 +0530

    Update garageconfig.js
    Edit connection string

--- garage/db/garageconfig.js
+++ garage/db/garageconfig.js
@@ -1,4 +1,4 @@
-// const mongoose=require('mongoose');
+ const mongoose=require('mongoose');
 // mongoose.connect('mongodb://localhost:27017/Garage');
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
