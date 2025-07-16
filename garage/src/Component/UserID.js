50f17be75870f438391d66c36da6cb7165ee08c5
JEET-HACKS <jitendrasuthar342@gmail.com>
2025-07-16 19:44:22 +0100

    Update UserID.js
    userid add to the userid div

--- garage/src/Component/UserID.js
+++ garage/src/Component/UserID.js
@@ -4,8 +4,10 @@ import {useNavigate} from 'react-router-dom';
 const UserId=({IsUser,onLoginSuccess,onClose})=>{
                        var userdiv= IsUser ? 'userdiv open' : 'userdiv';
                        var Redirect=useNavigate();
+                       const [username,Setusername]=React.useState('');
 
                        useEffect(()=>{
+                                      Setusername(localStorage.getItem("userid"));
                                     //document.querySelector('.userdiv.open').style.display='block';
                        })
 
@@ -36,10 +38,12 @@ const UserId=({IsUser,onLoginSuccess,onClose})=>{
                                 <div className="row innerdiv border m-3 border-dark rounded" >
                                       <div className="col-md-12 d-flex justify-content-start p-2  fw-bold">
                                            <label className="text-left" style={{'fontSize':'11px'}}>Emp Code:</label>
+                                           <label className="text-left" style={{'fontSize':'11px','marginLeft':'170px'}}>{username}</label>
                                           
                                       </div>
                                       <div className="col-md-12 d-flex justify-content-start p-2  fw-bold">
                                            <label className="text-left" style={{'fontSize':'11px'}}>User Group:</label>
+                                           <label className="text-left" style={{'fontSize':'11px','marginLeft':'163px'}}>{username}</label>
                                           
                                       </div>
                                       <div className="col-md-12 d-flex justify-content-start p-2 fw-bold">
